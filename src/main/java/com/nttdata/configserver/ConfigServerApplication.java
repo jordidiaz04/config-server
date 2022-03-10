1c8cc382d304c50c7200d1938be990ec3acc6772
Jordi Diaz <jordi.diaz04@hotmail.com>
2022-03-10 15:57:22 -0500

    Update code for future checkstyle plugin.

--- src/main/java/com/nttdata/configserver/ConfigServerApplication.java
+++ src/main/java/com/nttdata/configserver/ConfigServerApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Config-Server application, enable config server.
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigServerApplication.class, args);
+  }
 }
